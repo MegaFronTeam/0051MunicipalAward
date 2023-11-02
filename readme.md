8aba741560382fae09978262399942892b33bea1
Janis <janis.paberzs18@gmail.com>
2023-11-03 00:28:32 +0200

    add headerBlock

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # 0051MunicipalAward
  
+1. <https://megafronteam.github.io/0051MunicipalAward/public>
--- sourse/js/JSCCommon.js
+++ sourse/js/JSCCommon.js
@@ -92,11 +92,11 @@ class JSCCommon {
 			{ passive: true },
 		);
 
-		window.addEventListener('resize', () => {
-			if (window.matchMedia('(min-width: 992px)').matches) this.closeMenu();
-		},
-			{ passive: true },
-		);
+		// window.addEventListener('resize', () => {
+		// 	if (window.matchMedia('(min-width: 992px)').matches) this.closeMenu();
+		// },
+		// 	{ passive: true },
+		// );
 	}
 
 	// tabs  .
